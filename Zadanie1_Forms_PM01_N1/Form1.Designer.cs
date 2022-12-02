98960298e7b4a21ed85ade1a0a39190ad775f07f
SaveLuciferR <cmudrec@mail.ru>
2022-12-02 22:33:33 +0300

    Добавьте файлы проекта.

---
+++ Zadanie1_Forms_PM01_N1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Zadanie1_Forms_PM01_N1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(105, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Введите число";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(15, 28);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(126, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 16);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(216, 80);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Примечание: если число\r\nкратно 5, то оно будет делиться\r\nна 5, а если число являе" +
+    "тся\r\nлюбым другим, то оно будет\r\nувеличиваться на 2";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Location = new System.Drawing.Point(164, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(228, 110);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 60);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(91, 16);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Вывод числа";
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+			this.textBox2.Location = new System.Drawing.Point(15, 86);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(126, 22);
+			this.textBox2.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(290, 128);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(102, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Проверить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox3.Location = new System.Drawing.Point(15, 128);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.ShortcutsEnabled = false;
+			this.textBox3.Size = new System.Drawing.Size(269, 15);
+			this.textBox3.TabIndex = 7;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(411, 166);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Проверка числа";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox3;
+	}
+}
+
---
+++ Zadanie1_Forms_PM01_N1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie1_Forms_PM01_N1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		double f(double number)
+		{
+			return (number % 5 == 0) ? number / 5 : number + 1;
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				double number = double.Parse(textBox1.Text);
+				double res = f(number);
+				textBox2.Text = string.Format("{0}", res);
+			}
+			catch
+			{
+				textBox3.Text = string.Format("Введены некорректные данные.");
+			}
+			
+		}
+	}
+}
